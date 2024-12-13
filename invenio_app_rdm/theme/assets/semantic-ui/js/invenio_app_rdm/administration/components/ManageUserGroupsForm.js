f2cd76df3150cfbe910f67ccb53c63ff5f7c9d31
Jacob Collins <jacob.collins@ubiquitypress.com>
2024-12-13 17:12:14 +0000

    REPO-2454: Add ability to update groups associated for a user from user administration screen

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/administration/components/ManageUserGroupsForm.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/administration/components/ManageUserGroupsForm.js
@@ -12,7 +12,7 @@ import { i18next } from "@translations/invenio_app_rdm/i18next";
 import { Formik } from "formik";
 import _get from "lodash/get";
 
-import { withCancel, ErrorMessage} from "react-invenio-forms";
+import { withCancel, ErrorMessage, SelectField, ErrorLabel } from "react-invenio-forms";
 import { Form, Button, Modal, Message, Icon } from "semantic-ui-react";
 import { NotificationContext } from "@js/invenio_administration";
 import * as Yup from "yup";
@@ -25,6 +25,7 @@ export class ManageUserGroupsForm extends Component {
       loading: false,
       error: undefined,
       dropdownOptions: [],
+      userRoles: [],
     };
     this.confirmSchema = Yup.object({
       selectedOption: Yup.string()
@@ -33,50 +34,77 @@ export class ManageUserGroupsForm extends Component {
   }
 
   componentDidMount() {
+    console.log("Fetching dropdown options...");
     this.fetchDropdownOptions();
   }
 
-  // Fetch dropdown options from API
-  fetchDropdownOptions = async () => {
-    try {
-      const response =  UserModerationApi.groups() // Replace with your API URL
-      this.setState({
-        dropdownOptions: response.data.hits.hits, // Assume the API response contains an 'options' array
-        loading: false,
-      });
-    } catch (error) {
-      this.setState({
-        error: 'Failed to fetch options.',
-        loading: false,
-      });
-    }
+
+  getOptions = async (user_response, group_response) => {
+    const userGroupIds = user_response.hits.hits.map((item) => item.id);
+    return group_response.hits.hits.map((item, index) => ({
+      key: index,
+      text: userGroupIds.includes(item.id) 
+      ? `Remove '${item.name}'` 
+      : `Add '${item.name}'`,
+      value: userGroupIds.includes(item.id) 
+      ? `remove:${item.name}` 
+      : `add:${item.name}`,
+    }));
   };
 
-  componentWillUnmount() {
-    this.cancellableAction && this.cancellableAction.cancel();
-  }
+  // Fetch dropdown options from API
+  fetchDropdownOptions = async () => {
+    const { user } = this.props;
+    try {
+      const userResponse =  await UserModerationApi.userGroups(user) // Replace with your API URL
+      const response =  await UserModerationApi.groups() // Replace with your API URL
+      this.setState({
+        dropdownOptions: await this.getOptions(userResponse.data, response.data),
+        userRoles: userResponse.data.hits.hits.map(item => ({
+          name: item.name,
+          description: item.description
+        })),
+        loading: false,
+      });
+        } catch (error) {
+      this.setState({
+        error: 'Failed to fetch group options.',
+        loading: false,
+      });
+        }
+      };
 
-  static contextType = NotificationContext;
+      componentWillUnmount() {
+        this.cancellableAction && this.cancellableAction.cancel();
+      }
 
-  handleSubmit = async (event) => {
+      static contextType = NotificationContext;
+
+      handleSubmit = async (values) => {
     this.setState({ loading: true });
     const { addNotification } = this.context;
-    const { user, actionSuccessCallback } = this.props;
-    this.cancellableAction = withCancel(UserModerationApi.addGroupToUser(user, event.selectedOption.value));
+    const { user, actionCloseCallback } = this.props;
+    console.log(values.selectedOption)
+    const [action, group] = values.selectedOption.split(':');
+    const apiMethod = action === 'add' ? UserModerationApi.addGroupToUser : UserModerationApi.removeGroupFromUser;
+    this.cancellableAction = withCancel(apiMethod(user, group));
     try {
       await this.cancellableAction.promise;
       this.setState({ loading: false, error: undefined });
+      const successMessage = action === 'add' 
+        ? i18next.t("Added group to user successfully!") 
+        : i18next.t("Removed group from user successfully!");
       addNotification({
         title: i18next.t("Success"),
         content: i18next.t(
-          "Added group to user succesfully! You will be redirected in frontpage soon.",
+          successMessage,
           {
             id: user.id,
           }
         ),
         type: "success",
       });
-      actionSuccessCallback();
+      actionCloseCallback();
     } catch (error) {
       if (error === "UNMOUNTED") return;
 
@@ -88,83 +116,103 @@ export class ManageUserGroupsForm extends Component {
     }
   };
 
+
+  handleOnChange = ({ data, formikProps }) => {
+    formikProps.form.setFieldValue("selectedOption", data.value);
+  };
+
   handleModalClose = () => {
-    const { actionCancelCallback } = this.props;
-    actionCancelCallback();
+    const { actionCloseCallback } = this.props;
+    actionCloseCallback();
+  };
+
+
+  initFormValues = () => {
+    return {
+      selectedOption: ''
+    };
   };
 
   render() {
-    const { dropdownOptions, error, loading } = this.state;
+    const { userRoles, dropdownOptions, error, loading } = this.state;
     return (
       <Formik
         onSubmit={this.handleSubmit}
+        initialValues={this.initFormValues()}
         enableReinitialize
         validateOnChange={false}
         validateOnBlur={false}
         validationSchema={() => this.confirmSchema}
       >
-        {({ values, handleSubmit }) => {
-          return (
+      {({ values, handleSubmit }) => {
+            return (
             <>
               {error && (
-                <ErrorMessage
-                  header={i18next.t("Unable to impersonate user.")}
-                  content={i18next.t(error)}
-                  icon="exclamation"
-                  className="text-align-left"
-                  negative
-                />
+              <ErrorMessage
+                header={i18next.t("Unable to manage group")}
+                content={i18next.t(error)}
+                icon="exclamation"
+                className="text-align-left"
+                negative
+              />
               )}
 
               <Modal.Content>
-                <Message visible error>
-                  <p>
-                    <Icon name="warning sign" />
-                    {i18next.t(
-                      "Please read carefully and confirm the following statements before you proceed."
-                    )}
-                  </p>
-                </Message>
-                <Form>
-                  <label htmlFor="selectedOption">Choose a group:</label>
-                  <Field
-                    as="select"
-                    id="selectedOption"
-                    name="selectedOption"
-                  >
-                    <option value="" disabled>
-                      Select an option
-                    </option>
-                    {dropdownOptions.map((option) => (
-                      <option key={option.id} value={option.name}>
-                        {option.name}
-                      </option>
-                    ))}
-                  </Field>
-                  <ErrorMessage
-                    name="selectedOption"
-                    component="div"
-                    style={{ color: 'red' }}
+              <Message visible error>
+                <p>
+                <Icon name="warning sign" />
+                {i18next.t(
+                "Please be aware that you can give user access to administration groups."
+                )}
+                </p>
+              </Message>
+              <Form>
+                <Form.Field id="selectedOption">
+                  <SelectField
+                  label="Choose a group:"
+                  fieldPath="selectedOption"
+                  options={dropdownOptions}
+                  onChange={this.handleOnChange}
                   />
-                </Form>
-              </Modal.Content>
+                  <ErrorLabel
+                  role="alert"
+                  fieldPath="selectedOption"
+                  className="mt-0 mb-5"
+                  />
+                </Form.Field>
+              </Form>
+              <Message visible>
+                <h4>{i18next.t("Current User Roles")}</h4>
+                <ul>
+                {userRoles.length > 0 ? (
+                  userRoles.map((role, index) => (
+                  <li key={index}>
+                    <strong>{role.name}</strong>: {role.description}
+                  </li>
+                  ))
+                ) : (
+                  <li>{i18next.t("Not assigned to any groups yet.")}</li>
+                )}
+                </ul>
+              </Message>
               <Modal.Actions>
                 <Button onClick={this.handleModalClose} floated="left">
-                  Close
+                Close
                 </Button>
-                <Button
-                  size="small"
-                  labelPosition="left"
-                  icon="check"
-                  color="green"
-                  content={i18next.t("Add Group")}
-                  onClick={(event) => handleSubmit(event)}
-                  loading={loading}
-                  disabled={loading}
-                />
+                <Button size="small"
+                type="submit"
+                labelPosition="left"
+                icon="check"
+                color="green"
+                content={i18next.t("Update Group")}
+                onClick={(event) => handleSubmit(event)}
+                loading={loading}
+                disabled={loading}>
+                </Button>
               </Modal.Actions>
-            </>
-          );
+              </Modal.Content>
+              </>
+              );
         }}
       </Formik>
     );
@@ -173,6 +221,5 @@ export class ManageUserGroupsForm extends Component {
 
 ManageUserGroupsForm.propTypes = {
   user: PropTypes.object.isRequired,
-  actionSuccessCallback: PropTypes.func.isRequired,
-  actionCancelCallback: PropTypes.func.isRequired,
+  actionCloseCallback: PropTypes.func.isRequired,
 };
