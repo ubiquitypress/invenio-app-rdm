56424120503b1935b1c94cad68899ea0a8f108c4
Jacob Collins <jacob.collins@ubiquitypress.com>
2024-12-13 17:10:44 +0000

    REPO-2454: Add ability to update groups associated for a user from user administration screen

---
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/administration/components/ManageUserGroups.js
@@ -0,0 +1,92 @@
+
+/*
+ * // This file is part of Invenio-App-Rdm
+ * // Copyright (C) 2023 CERN.
+ * //
+ * // Invenio-App-Rdm is free software; you can redistribute it and/or modify it
+ * // under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Modal, Icon, Dropdown } from "semantic-ui-react";
+import { ActionModal } from "@js/invenio_administration";
+import _isEmpty from "lodash/isEmpty";
+import { ManageUserGroupsForm } from "./ManageUserGroupsForm";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
+
+export class ManageUserGroups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false,
+      modalHeader: undefined,
+      modalBody: undefined,
+    };
+  }
+
+  onModalTriggerClick = (e) => {
+    const { user } = this.props;
+
+    this.setState({
+      modalOpen: true,
+      modalHeader: i18next.t("Manage user groups"),
+      modalBody: (
+        <ManageUserGroupsForm
+          actionSuccessCallback={this.handleSuccess}
+          actionCancelCallback={this.closeModal}
+          user={user}
+        />
+      ),
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      modalOpen: false,
+      modalHeader: undefined,
+      modalBody: undefined,
+    });
+  };
+
+  handleSuccess = () => {
+    const { successCallback } = this.props;
+    this.setState({
+      modalOpen: false,
+      modalHeader: undefined,
+      modalBody: undefined,
+    });
+    successCallback();
+  };
+
+  render() {
+    const { user } = this.props;
+    const { modalOpen, modalHeader, modalBody } = this.state;
+    return (
+      <>
+        <Dropdown.Item
+          key="add-group-to-user"
+          onClick={this.onModalTriggerClick}
+          icon
+          fluid
+          basic
+          labelPosition="left"
+        >
+          <Icon name="spy" />
+          Add Group
+        </Dropdown.Item>
+        <ActionModal modalOpen={modalOpen} user={user}>
+          {modalHeader && <Modal.Header>{modalHeader}</Modal.Header>}
+          {!_isEmpty(modalBody) && modalBody}
+        </ActionModal>
+      </>
+    );
+  }
+}
+
+ManageUserGroups.propTypes = {
+  user: PropTypes.object.isRequired,
+  successCallback: PropTypes.func.isRequired,
+};
+
+ManageUserGroups.defaultProps = {};
